c388f48b537f765f912850e68e85f323c5f18537
Leon Carswell <aleon@Leons-MacBook-Air.local>
2017-10-12 14:30:06 -0400

    hitting each current route with success

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import HomePage from './components/home/HomePage'
+import IdeaPage from './components/idea/IdeaPage'
+import LogInPage from './components/login/LogInPage'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <div>
+            <span>
+              <Link to="/">Home</Link>
+              <Link to="/idea">Idea</Link>
+              <Link to="/login">Login</Link>
+            </span>
+          </div>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/idea" component={IdeaPage} />
+            <Route exact path="/login" component={LogInPage} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ client/src/components/login/LogInPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LogInPage extends Component {
+    render() {
+        return (
+            <div>
+               <h1> hellleerrr from login page </h1> 
+            </div>
+        );
+    }
+}
+
+export default LogInPage;
